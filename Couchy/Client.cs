2302ed6f732e258c3860352eb7e43c038ed146b3
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-12-05 01:11:15 +0100

    chore: text formatting

--- Couchy/Client.cs
+++ Couchy/Client.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Data;
 using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
 
 namespace Couchy
 {
